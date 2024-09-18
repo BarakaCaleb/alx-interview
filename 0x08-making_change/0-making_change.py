6ff630a812c184c6f6ab4751590dd6e2ff014d08
BarakaCaleb <calebbaraka79@gmail.com>
2024-09-18 12:35:31 +0300

    more updates

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
+"""
+Interview Question on: fewest number of coins needed to
+meet a given amount total
+"""
+
 def makeChange(coins, total):
     """
     Given a pile of coins of different values,
