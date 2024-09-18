4730fafb3bf9003ab6b11b0f9e9f122f6f60bd2f
BarakaCaleb <calebbaraka79@gmail.com>
2024-09-18 12:17:13 +0300

    Task: created the fucntion

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def makeChange(coins, total):
+    """
+    Given a pile of coins of different values, determine the fewest number of coins needed to meet a given amount total.
+    """
+    if total <= 0:
+        return 0
+    coins.sort(reverse=True)
+    coin_count = 0
+    for coin in coins:
+        if total <= 0:
+            break
+        coin_count += total // coin
+        total = total % coin
+    if total != 0:
+        return -1
+    return coin_count
